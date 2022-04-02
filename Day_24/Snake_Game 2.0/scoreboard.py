fbdd80822be232c4afc7574bafb2238f1e0b92c2
MagicTor <yussif.jaber@live.com>
2022-04-02 13:04:30 +0300

    Completed Part 1 of Challenge Day 24. Completed Snake_Game 2.0
    Added the ability to save high scores into a file

---
+++ Day_24/Snake_Game 2.0/scoreboard.py
@@ -0,0 +1,45 @@
+import turtle
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ('Courier', 14, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.get_high_score()
+        self.penup()
+        self.goto(0, 270)
+        self.color("white")
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.save_high_score()
+        self.score = 0
+        self.update_scoreboard()
+
+    def get_high_score(self):
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
+
+    def save_high_score(self):
+        with open("data.txt", "w") as file:
+            file.write(str(self.high_score))
+
+#    def game_over(self):
+#        self.goto(0, 0)
+#        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
---
+++ Day_24/Snake_Game 2.0/snake.py
@@ -0,0 +1,56 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP, DOWN, LEFT, RIGHT = 90, 270, 180, 0
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.speed("fastest")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for i in range(len(self.segments) - 1, 0, -1):
+            new_pos = self.segments[i - 1].position()
+            self.segments[i].goto(new_pos)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.reset()
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
