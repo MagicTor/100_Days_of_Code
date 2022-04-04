14ba7b2ce67a354ef106136da71bb049209abfed
MagicTor <yussif.jaber@live.com>
2022-04-04 22:51:22 +0300

    Optimized part 1 of Day 25.
    Panda has a Series method called .value_counts() that takes the total count for each type of item in the column, which is exactly what my 9 lines of code did before.

--- Day_25/Squirrel_Data_Analysis/main.py
+++ Day_25/Squirrel_Data_Analysis/main.py
@@ -2,15 +2,18 @@
 import pandas
 
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-
-gray_count = len(data[data["Primary Fur Color"] == "Gray"])
-red_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
-black_count = len(data[data["Primary Fur Color"] == "Black"])
-
-data_dict = {
-    "Fur Color": ["grey","red","black"],
-    "Count": [gray_count, red_count, black_count]
-}
-
-df = pandas.DataFrame(data_dict)
+df = data["Primary Fur Color"].value_counts()
 df.to_csv("squirrel_color_counts.csv")
+#
+# gray_count = len(data[data["Primary Fur Color"] == "Gray"])
+# red_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+# black_count = len(data[data["Primary Fur Color"] == "Black"])
+#
+# data_dict = {
+#     "Fur Color": ["grey","red","black"],
+#     "Count": [gray_count, red_count, black_count]
+# }
+#
+# df = pandas.DataFrame(data_dict)
+# df.to_csv("squirrel_color_counts.csv")
+
