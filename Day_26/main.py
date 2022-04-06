d3f30b507ff782b60afbcf8e60e01cdc945c5635
MagicTor <yussif.jaber@live.com>
2022-04-07 00:25:52 +0300

    Day 26 Complete Created a phonetic alphabet converter from user generated input using Pandas.

---
+++ Day_26/main.py
@@ -0,0 +1,8 @@
+import pandas
+
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+
+user_word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in user_word]
+print(output_list)
