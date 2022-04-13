4c42aa7ce60718bd73534aeb89261d2f66bb8970
MagicTor <yussif.jaber@live.com>
2022-04-14 02:17:46 +0300

    - Changed the password generating algorithm from user input and the reduction method to creating the most secure password given a password length. - Object Oriented the code as well

--- Day_05/Password_Manager.py
+++ Day_05/Password_Manager.py
@@ -1,4 +1,5 @@
 # Password Generator Project
+import math
 import random
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
@@ -8,36 +9,25 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '+', '-', '_', '=', '(', ')', '{', '}', '[', ']', '\\',
            '/', '|', '.', ',', ':', ';', '<', '>', '?']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+password_characters = letters + numbers + symbols
 
+class password_generator:
 
-password = []
-while nr_numbers + nr_symbols + nr_letters > 0:  # Running the loop until there are no more characters in the password to choose from
+    def __init__(self):
+        self.password = self.generate_password()
 
-    # Create a new list with all possible characters
-    new_list = []
-    if nr_letters > 0:
-        new_list += letters
-    if nr_numbers > 0:
-        new_list += numbers
-    if nr_symbols > 0:
-        new_list += symbols
+        print(self.generate_password())
 
-    # Get a random character from the new list
-    password_character = random.choice(new_list)
+    def generate_password(self, pass_length=14):
+        if pass_length > 2:
+            nr_numbers = math.ceil(0.109 * pass_length)
+            nr_symbols = math.ceil(0.326 * pass_length)
+            nr_letters = pass_length - nr_numbers - nr_symbols
+            password_list = [random.choice(numbers) for char in range(nr_numbers)]
+            password_list += [random.choice(symbols) for char in range(nr_symbols)]
+            password_list += [random.choice(letters) for char in range(nr_letters)]
+            random.shuffle(password_list)
+        else:
+            password_list = [random.choice(password_characters) for char in range(pass_length)]
 
-    # Update the password
-    password.append(password_character)
-
-    # Shorten the password length by either 1 letter, symbol, or number
-    if password_character in numbers:
-        nr_numbers -= 1
-    elif password_character in symbols:
-        nr_symbols -= 1
-    else:               # Else it will be in the letters list
-        nr_letters -= 1
-
-print("".join(password))
+        return "".join(password_list)
