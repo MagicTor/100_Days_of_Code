9f02634e7d50e05be5150bc0acabfe8717591aab
MagicTor <yussif.jaber@live.com>
2022-04-14 02:21:40 +0300

    - Reorganized some lines of code for cleaner reading and removed a line

--- Day_05/Password_Manager.py
+++ Day_05/Password_Manager.py
@@ -2,12 +2,12 @@
 import math
 import random
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-           'w', 'x', 'y', 'z']
-letters += [letter.upper() for letter in letters]
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '+', '-', '_', '=', '(', ')', '{', '}', '[', ']', '\\',
            '/', '|', '.', ',', ':', ';', '<', '>', '?']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z']
+letters += [letter.upper() for letter in letters]
 
 password_characters = letters + numbers + symbols
 
@@ -15,8 +15,6 @@ class password_generator:
 
     def __init__(self):
         self.password = self.generate_password()
-
-        print(self.generate_password())
 
     def generate_password(self, pass_length=14):
         if pass_length > 2:
